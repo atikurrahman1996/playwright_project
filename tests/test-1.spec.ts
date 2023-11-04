d22b34b81be231f13d1da362763ab98614f50d5d
Atikur Rahman <atik.sqa.connexpay@gmail.com>
2023-11-04 12:36:45 +0600

    codegen with terminal and vs code extenstion

---
+++ tests/test-1.spec.ts
@@ -0,0 +1,12 @@
+import { test, expect } from '@playwright/test';
+
+test('test', async ({ page }) => {
+  await page.goto('https://www.demoblaze.com/');
+  await page.getByRole('link', { name: 'Log in' }).click();
+  await page.locator('#loginusername').click();
+  await page.locator('#loginusername').fill('Atikur Rahman');
+  await page.locator('#loginpassword').click();
+  await page.locator('#loginpassword').fill('Atik@123');
+  await page.getByRole('button', { name: 'Log in' }).click();
+  await page.getByRole('link', { name: 'Log out' }).click();
+});
