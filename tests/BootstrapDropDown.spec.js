b9936f7f9d25397166f0ed92bb0de6908cf72ec8
Atikur Rahman <atikur2013rahman@gmail.com>
2023-11-11 23:25:22 +0600

    bootstrap dropdowns

--- tests/BootstrapDropDown.spec.js
+++ tests/BootstrapDropDown.spec.js
@@ -4,9 +4,19 @@ test("country dropdown", async ({ page }) => {
   await page.goto("http://autopract.com/selenium/dropdown4/");
   await page.locator("button.dropdown-toggle").click();
 
+  // select text Bangladesh
+
   await page
     .locator("ul.dropdown-menu")
     .locator("li")
     .filter({ hasText: "Bangladesh" })
     .click();
+
+  // print all the drop down value
+
+  const options = await page.$$("ul>li");
+  for (let option of options) {
+    const value = await option.textContent();
+    console.log("value is", value);
+  }
 });
