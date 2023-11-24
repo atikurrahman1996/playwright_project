2a8424082002b9b3d918ddf3ebbf4d7ca32ac8dd
Atikur Rahman <atikur2013rahman@gmail.com>
2023-11-24 09:54:18 +0600

    taking screenshoot

---
+++ tests/Screenshoot.spec.js
@@ -0,0 +1,24 @@
+import { test, expect } from "@playwright/test";
+
+test("page screenshot", async ({ page }) => {
+  await page.goto("https://www.opencart.com/index.php?route=cms/demo");
+  //await page.screenshot({ path: "tests/Screenshoot/" + "HomePage.png" }); // default screenshot will save project folder
+  await page.screenshot({
+    path: "tests/Screenshoot/" + Date.now() + "HomePage.png",
+  }); // screenshot will save with different name if we take multiple screenshoot
+});
+test("Full page screenshot", async ({ page }) => {
+  await page.goto("https://www.opencart.com/index.php?route=cms/demo");
+
+  await page.screenshot({
+    path: "tests/Screenshoot/" + Date.now() + "FullPage.png",
+    fullPage: true,
+  });
+});
+test.only("Elements screenshot", async ({ page }) => {
+  await page.goto("https://www.opencart.com/index.php?route=cms/demo");
+
+  await page.locator("//a[@href='https://demo.opencart.com/']").screenshot({
+    path: "tests/Screenshoot/" + Date.now() + "StorePage.png",
+  });
+});
